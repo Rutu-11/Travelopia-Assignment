a9558f9e2bfb0c751d3476114a01a98d52e445d1
Rutuja Dhekolkar <rutujadhekolkar97@gmail.com>
2023-05-27 14:01:16 +0530

    Done with Backend

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const router = require('./routes/routes')
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use('/api',router)
 // Connect to MongoDB
 mongoose.connect(`mongodb+srv://rutujadhekolkar97:${process.env.MONGODB_PASSWORD}@cluster0.pdssvjd.mongodb.net/?retryWrites=true&w=majority`, {
   useNewUrlParser: true,

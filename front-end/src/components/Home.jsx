1610234bccb0dceeb2d6cb991b6c879c73c82cfc
Rutuja Dhekolkar <rutujadhekolkar97@gmail.com>
2023-05-27 15:13:50 +0530

    git push origin main

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
+import React from 'react';
+import './common.css';
+import wallpaper from '../assets/wallpaper.webp'
+const styles = {
+    background: `url(${wallpaper})`,
+    
+    /* Additional background properties */
+  };
 function Home(){
     return(
-        <div>Home</div>
+        <div  className='container'>Lorem*34</div>
     )
 }
 
